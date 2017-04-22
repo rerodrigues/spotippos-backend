548ac0cd058407f3ab7d5bfd440ac5675b0f72da
Renato Rodrigues <renato.sp@gmail.com>
2017-04-21 21:15:06 -0300

    Added CORS

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 require('./models/db');
 
@@ -18,6 +19,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in ./public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
